7a4fd6225b6adfe86dac345a454a49b3ff494ccc
cc <12212739@mail.sustech.edu.cn>
2024-12-23 04:05:14 +0800

    老架构建议抛弃，这是我的新方案

--- src/conf_server.py
+++ src/conf_server.py
@@ -1,169 +1,167 @@
 import asyncio
-from config import *
+import aiosip
+import aiortsp
+from util import *
 
-
+# ConferenceServer 需要设置为RTP服务器，就是还没搞懂clients_info和client_conns是干啥的
 class ConferenceServer:
-    def __init__(self, conference_id, conf_serve_port, data_serve_ports):
-        self.conference_id = conference_id
-        self.conf_serve_port = conf_serve_port
-        self.data_serve_ports = data_serve_ports
-        self.data_types = list(data_serve_ports.keys())
-        self.clients_info = {}  # {client_addr: writer}
-        self.client_conns = {}  # {data_type: {client_addr: writer}}
-        self.running = True
+    def __init__(self, ):
+        # async server
+        self.conference_id = None  # conference_id for distinguish difference conference
+        self.conf_serve_ports = None
+        self.data_serve_ports = {}
+        self.data_types = ['screen', 'camera', 'audio']  # example data types in a video conference
+        self.clients_info = None
+        self.client_conns = None
+        self.mode = 'Client-Server'  # or 'P2P' if you want to support peer-to-peer conference mode
 
     async def handle_data(self, reader, writer, data_type):
-        addr = writer.get_extra_info('peername')
-        port = writer.get_extra_info('sockname')[1]
-        if data_type not in self.client_conns:
-            self.client_conns[data_type] = {}
-        self.client_conns[data_type][addr] = writer
-        print(f'[Data] {data_type} connection from {addr} on port {port}')
-
-        try:
-            while self.running:
-                data = await reader.read(1024*1024)
-                if not data:
-                    break
-                # 转发数据给其他客户端
-                for client_addr, client_writer in self.client_conns[data_type].items():
-                    if client_addr != addr:
-                        client_writer.write(data)
-                        await client_writer.drain()
-        except ConnectionResetError:
-            pass
-        finally:
-            print(f'[Data] {data_type} connection closed from {addr}')
-            del self.client_conns[data_type][addr]
-            writer.close()
-            await writer.wait_closed()
+        """
+        running task: receive sharing stream data from a client and decide how to forward them to the rest clients
+        """
 
     async def handle_client(self, reader, writer):
-        addr = writer.get_extra_info('peername')
-        self.clients_info[addr] = writer
-        print(f'[Conference] Client connected: {addr}')
+        """
+        running task: handle the in-meeting requests or messages from clients
+        """
 
-        try:
-            while self.running:
-                data = await reader.readline()
-                if not data:
-                    break
-                message = data.decode().strip()
-                print(f'Received from {addr}: {message}')
-                # 处理会议中的请求（如退出等）
-                if message == 'quit':
-                    break
-        except ConnectionResetError:
-            pass
-        finally:
-            print(f'[Conference] Client disconnected: {addr}')
-            del self.clients_info[addr]
-            writer.close()
-            await writer.wait_closed()
+    async def log(self):
+        while self.running:
+            print('Something about server status')
+            await asyncio.sleep(LOG_INTERVAL)
 
-    async def start(self):
-        # 启动会议控制服务器
-        self.conf_server = await asyncio.start_server(self.handle_client, SERVER_IP, self.conf_serve_port)
-        print(f'ConferenceServer started on port {self.conf_serve_port}')
+    async def cancel_conference(self):
+        """
+        handle cancel conference request: disconnect all connections to cancel the conference
+        """
 
-        # 启动数据服务器
-        self.data_servers = []
-        for data_type, port in self.data_serve_ports.items():
-            server = await asyncio.start_server(
-                lambda r, w, dt=data_type: self.handle_data(r, w, dt), SERVER_IP, port)
-            self.data_servers.append(server)
-            print(f'Data server for {data_type} started on port {port}')
-
-        await self.conf_server.serve_forever()
-
-    async def stop(self):
-        self.running = False
-        self.conf_server.close()
-        await self.conf_server.wait_closed()
-        for server in self.data_servers:
-            server.close()
-            await server.wait_closed()
-        print('ConferenceServer stopped')
+    def start(self):
+        '''
+        start the ConferenceServer and necessary running tasks to handle clients in this conference
+        '''
 
 
 class MainServer:
     def __init__(self, server_ip, main_port):
         self.server_ip = server_ip
         self.server_port = main_port
-        self.conference_servers = {}
-        self.next_conference_id = 1
+        self.conference_servers = {}  # conference_id -> ConferenceServer
+        self.app = aiosip.Application()
 
-    async def handle_create_conference(self, _, writer):
-        conference_id = self.next_conference_id
-        self.next_conference_id += 1
+    async def handle_create_conference(self, request, message):
+        """
+        create conference: create and start the corresponding ConferenceServer, and reply necessary info to client
+        """
+        conference_id = len(self.conference_servers) + 1
+        # Create a new conference server
+        port = 5000 + len(self.conference_servers)  # Assign a unique port for the conference
+        conf_server = ConferenceServer(conference_id, self.server_ip, port)
+        self.conference_servers[conference_id] = conf_server
+        asyncio.create_task(conf_server.start())
 
-        # 分配端口（简单实现，可以根据需要修改）
-        conf_serve_port = MAIN_SERVER_PORT + conference_id * 10
-        data_serve_ports = {
-            'screen': conf_serve_port + 1,
-            # 'camera': conf_serve_port + 2,
-            # 'audio': conf_serve_port + 3,
-        }
+        conf_server = self.conference_servers[conference_id]
 
-        # 创建并启动 ConferenceServer
-        conference_server = ConferenceServer(conference_id, conf_serve_port, data_serve_ports)
-        asyncio.create_task(conference_server.start())
-        self.conference_servers[conference_id] = conference_server
+        # Respond with conference details (host and port)
+        response = aiosip.Response.from_request(request, status_code=200, reason="OK")
+        response.headers['RTP-Host'] = self.server_ip
+        response.headers['RTP-Port'] = str(conf_server.port)
+        await request.send_response(response)
+        print(f"[MainServer] Successfully created conference {conference_id}.")
 
-        response = f'CREATE_OK {conference_id} {conf_serve_port} {data_serve_ports}\n'
-        writer.write(response.encode())
-        await writer.drain()
-        print(f'Conference {conference_id} created')
-        writer.close()
-        await writer.wait_closed()
+    async def handle_join_conference(self, request, message):
+        """
+        join conference: search corresponding conference_info and ConferenceServer, and reply necessary info to client
+        """
+        conference_id = message.headers.get('Conference-ID', None)
+        if conference_id not in self.conference_servers:
+            response = aiosip.Response.from_request(request, status_code=404, reason=f"Conference {conference_id} not found")
+            await request.send_response(response)
+            print(f"[MainServer] Error: conference {conference_id} not found when handling JOIN request.")
+        else:
+            conf_server = self.conference_servers[conference_id]
+            response = aiosip.Response.from_request(request, status_code=200, reason="OK")
+            response.headers['RTP-Host'] = self.server_ip
+            response.headers['RTP-Port'] = str(conf_server.port)
+            await request.send_response(response)
+            print(f"[MainServer] JOIN {conference_id} handled.")
 
-    async def handle_join_conference(self, _, writer, conference_id):
+    async def handle_quit_conference(self, request, message):
+        """
+        quit conference (in-meeting request & or no need to request)
+        """
+        conference_id = message.headers.get('Conference-ID', None)
         if conference_id in self.conference_servers:
             conference_server = self.conference_servers[conference_id]
-            response = f'JOIN_OK {conference_id} {conference_server.conf_serve_port} {conference_server.data_serve_ports}\n'
-            writer.write(response.encode())
-            await writer.drain()
-        else:
-            writer.write('ERROR Conference not found\n'.encode())
-            await writer.drain()
-        writer.close()
-        await writer.wait_closed()
+            if len(conference_server.clients) == 1:
+                # Close the conference server if there is no client in the conference
+                del self.conference_servers[conference_id]
+                response = aiosip.Response.from_request(request, status_code=200, reason="OK")
+                await request.send_response(response)
+                print(f"[MainServer] Quit {conference_id} handled and conference is canceled.")
+            else:
+# -------------------------------------------这里还没写完-------------------------------------------------
 
-    async def request_handler(self, reader, writer):
-        data = await reader.readline()
-        message = data.decode().strip()
-        print(f'Received: {message}')
-        if message == 'CREATE_CONFERENCE':
-            await self.handle_create_conference(reader, writer)
-        elif message.startswith('JOIN_CONFERENCE'):
-            _, conf_id = message.split()
-            await self.handle_join_conference(reader, writer, int(conf_id))
-        else:
-            writer.write('ERROR Invalid command\n'.encode())
-            await writer.drain()
-            writer.close()
-            await writer.wait_closed()
 
-    def start(self):
+
+
+
+                
+                response = aiosip.Response.from_request(request, status_code=200, reason="OK")
+                await request.send_response(response)
+                print(f"[MainServer] Quit {conference_id} handled.")
+        else:
+            response = aiosip.Response.from_request(request, status_code=404, reason=f"Conference {conference_id} not found")
+            await request.send_response(response)
+            print(f"[MainServer] Error: conference {conference_id} not found when handling QUIT request.")
+
+    async def handle_cancel_conference(self, request, message):
+        """
+        cancel conference (in-meeting request, a ConferenceServer should be closed by the MainServer)
+        """
+        conference_id = message.headers.get('Conference-ID', None)
+        del self.conference_servers[conference_id]
+        response = aiosip.Response.from_request(request, status_code=200, reason="OK")
+        await request.send_response(response)
+        print(f"[MainServer] Conference {conference_id} is canceled.")
+
+    async def request_handler(self, protocol, message):
+        """
+        running task: handle out-meeting (or also in-meeting) requests from clients
+        """
+        if message.method == 'CREATE':
+            await self.handle_create_conference(protocol, message)
+        elif message.method == 'QUIT':
+            await self.handle_quit_conference(protocol, message)
+        elif message.method == 'JOIN':
+            await self.handle_join_conference(protocol, message)
+        elif message.method == 'CANCEL':
+            await self.handle_cancel_conference(protocol, message)
+        else:
+            response = aiosip.Response.from_request(protocol, status_code=501, reason="Not Implemented")
+            await protocol.send_response(response)
+
+    async def start(self):
+        """
+        Start the SIP server to handle requests.
+        """
+        print(f"[MainServer] Starting SIP server on {self.server_ip}:{self.server_port}")
+        await self.app.listen(self.server_ip, self.server_port, self.request_handler)
+
+    def run(self):
+        """
+        Run the main server.
+        """
         loop = asyncio.get_event_loop()
-        coro = asyncio.start_server(self.request_handler, self.server_ip, self.server_port)
-        server = loop.run_until_complete(coro)
-        print(f'MainServer started on {self.server_ip}:{self.server_port}')
         try:
+            loop.run_until_complete(self.start())
             loop.run_forever()
         except KeyboardInterrupt:
-            pass
-        server.close()
-        loop.run_until_complete(server.wait_closed())
-        loop.run_until_complete(self.stop_all_conferences())
-        loop.close()
-
-    async def stop_all_conferences(self):
-        for server in self.conference_servers.values():
-            await server.stop()
-        print('All conferences stopped')
+            print("[MainServer] Shutting down...")
+        finally:
+            loop.run_until_complete(self.app.close())
+            loop.close()
 
 
 if __name__ == '__main__':
     server = MainServer(SERVER_IP, MAIN_SERVER_PORT)
-    server.start()
+    server.run()
